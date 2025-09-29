775c95046ef43bd1a6597d3e59b1d167ff79b361
AlexandraDeGracia <alexandra.degracia1@utp.ac.pa>
2025-09-28 21:12:05 -0500

    video del 12 al 19 programa orientado a objetos

---
+++ video del 12 al 19/Superheroapp/Superheroapp/Models/AntiHeroe.cs
@@ -0,0 +1,17 @@
+﻿using SuperheroesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroapp.Models
+{
+    internal class AntiHeroe : SuperHeroe
+    {
+        public string RealizarAccionDeAntiheroe(string accion)
+        {
+            return $"El AntiHeroe {NombreEIdentidadSecreta} a realizado {accion}";
+        }
+    }
+}
---
+++ video del 12 al 19/Superheroapp/Superheroapp/Models/SuperPoder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheroapp.Models
+{
+    internal class SuperPoder
+    {
+        public string nombre;
+        public string description;
+        public NivelPoder Nivel;
+
+        public SuperPoder()
+        {
+            Nivel = NivelPoder.NivelUno;
+        }
+    }
+}
