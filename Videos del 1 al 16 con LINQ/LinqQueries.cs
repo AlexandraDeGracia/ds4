50fec85cbe06d9eaeb20b5e68b1034e85cbd129a
AlexandraDeGracia <alexandra.degracia1@utp.ac.pa>
2025-10-05 20:08:54 -0500

    videos del 1 al 16

---
+++ Videos del 1 al 16 con LINQ/LinqQueries.cs
@@ -0,0 +1,52 @@
+public class LinqQueries
+{
+    private List<Book> librosCollection = new List<Book>();
+
+    public LinqQueries()
+    {
+        using(StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+        }
+    }
+
+    public IEnumerable<Book> TodaLaColeccion()
+    {
+        return librosCollection;
+    }
+
+    public IEnumerable<Book> LibrosDespuesdel2000()
+    {
+        //extension method
+        //return librosCollection.Where(p=> p.PublishedDate.Year > 2000);
+
+        //query expresion
+
+        return from l in librosCollection where l.PublishedDate.Year > 2000 select l;
+    }
+
+    public IEnumerable<Book> LibrosConMasde250PagConPalabrasInAction()
+    {
+        //extension methods
+        //return librosCollection.Where(p=> p.PageCount > 250 && p.Title.Contains("in Action"));
+
+        //query expression
+        return from l in librosCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
+    }
+
+    public bool TodosLosLibrosTienenStatus()
+    {
+        return librosCollection.All(p=> p.Status!= string.Empty);
+    }
+
+    public bool SiAlgunLibroFuePublicado2005()
+    {
+        return librosCollection.Any(p=> p.PublishedDate.Year == 2005);
+    }
+
+    public IEnumerable<Book> LibrosdePython()
+    {
+        return librosCollection.Where(p=> p.Categories.Contains("Python"));
+    }
+}
