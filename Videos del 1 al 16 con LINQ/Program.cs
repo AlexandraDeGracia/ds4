a2c233c542dd681f5f5958b6a1596b43c4a2d97d
AlexandraDeGracia <alexandra.degracia1@utp.ac.pa>
2025-10-05 21:45:33 -0500

    video 1 al 16

---
+++ Videos del 1 al 16 con LINQ/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo!");
+        Console.ReadKey(); // opcional: espera que presiones una tecla antes de cerrar
+    }
+}
